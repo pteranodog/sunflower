afad6cd8a22cea85c869fd9fe3854205d9fc18ad
pteranodog <pteranodog@gmail.com>
2022-09-27 17:37:53 -0500

    first test of Teensy

--- sunflower-teensy/src/main.cpp
+++ sunflower-teensy/src/main.cpp
@@ -1,4 +1,10 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP3XX.h>
+#include <Adafruit_BNO08x.h>
+
+int led = 13;
 
 void getSensorData() {
   // 
@@ -13,9 +19,17 @@ void writeTelemetry() {
 }
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  Wire.begin();
+  Serial.begin(115200);
+  Serial1.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(250);
+  digitalWrite(led, LOW);
+  delay(250);
+  Serial1.println("Hello World! from the loop!");
+  Serial.println("Hello World! from the loop!");
 }
