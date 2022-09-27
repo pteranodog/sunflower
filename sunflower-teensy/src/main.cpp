c8b2859c302dd44ab64b56eb61dd8221d347429e
pteranodog <pteranodog@gmail.com>
2022-09-27 18:08:27 -0500

    imu test

--- sunflower-teensy/src/main.cpp
+++ sunflower-teensy/src/main.cpp
@@ -5,6 +5,8 @@
 #include <Adafruit_BNO08x.h>
 
 int led = 13;
+Adafruit_BNO08x imu = Adafruit_BNO08x();
+sh2_SensorValue_t imuData;
 
 void getSensorData() {
   // 
@@ -22,14 +24,18 @@ void setup() {
   pinMode(led, OUTPUT);
   Wire.begin();
   Serial.begin(115200);
-  Serial1.begin(115200);
+  imu.begin_I2C();
 }
 
 void loop() {
   digitalWrite(led, HIGH);
-  delay(250);
+  delay(50);
   digitalWrite(led, LOW);
-  delay(250);
-  Serial1.println("Hello World! from the loop!");
-  Serial.println("Hello World! from the loop!");
+  delay(50);
+  imu.getSensorEvent(&imuData);
+  Serial.print(imuData.un.accelerometer.x);
+  Serial.print(" ");
+  Serial.print(imuData.un.accelerometer.y);
+  Serial.print(" ");
+  Serial.println(imuData.un.accelerometer.z);
 }
